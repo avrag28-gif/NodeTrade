70d6508d3ebfde21e2efc64480c6bde8f38ae2f4
avrag28-gif <avrag28@gmail.com>
2026-09-03 19:48:59 +0700

    Export core model and execution interfaces

--- src/nodetrade/__init__.py
+++ src/nodetrade/__init__.py
@@ -1,5 +1,7 @@
-"""NodeTrade adaptive XAUUSD research engine."""
+"""NodeTrade adaptive XAUUSD trading engine."""
 
 from .engine import NodeTradeEngine
+from .execution_engine import BrokerAdapter, OrderRequest, PaperBroker, Side
+from .model import CausalDirectionModel
 
-__all__ = ["NodeTradeEngine"]
+__all__ = ["NodeTradeEngine", "CausalDirectionModel", "BrokerAdapter", "OrderRequest", "PaperBroker", "Side"]
